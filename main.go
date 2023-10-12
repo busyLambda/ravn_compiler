089f7bf2be500a0dfd5d4429d305a457e73c506e
Susa Milán <mailtosnek@protonmail.com>
2023-10-12 20:05:31 +0200

    Simple repl

--- main.go
+++ main.go
@@ -3,46 +3,67 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 
 	"github.com/busylambda/raven/parser"
 )
 
 func main() {
-  file, err := os.ReadFile("main.rvn")
-  if err != nil {
-    fmt.Println("Error reading file: ", err)
-    panic(err)
-  }
+	// file, err := os.ReadFile("main.rvn")
+	// if err != nil {
+	// 	fmt.Println("Error reading file: ", err)
+	// 	panic(err)
+	// }
 
-  scanner := parser.NewScanner(string(file))
-  for {
-    tokenKind, literal := scanner.Scan()
-    if tokenKind == parser.EOF {
-      break
-    }
-    
-    token := parser.NewToken(tokenKind, literal)
+	// scanner := parser.NewScanner(string(file))
+	// for {
+	// 	tokenKind, literal := scanner.Scan()
+	// 	if tokenKind == parser.EOF {
+	// 		break
+	// 	}
 
-    fmt.Println(token.String())
-  }
+	// 	token := parser.NewToken(tokenKind, literal)
 
-  repl()
+	// 	fmt.Println(token.String())
+	// }
+	// Write a repl for the lexer
 
-  fmt.Println("Pull the trigger!")
+	for {
+		repl()
+		var command string
+		fmt.Scanln(&command)
+
+		switch command {
+		case "clear":
+			clearScreen()
+		case "exit":
+			os.Exit(0)
+		case "help":
+			fmt.Println("Commands: clear, exit, help")
+		default:
+			scanner := parser.NewScanner(command)
+
+			for {
+				tokenKind, literal := scanner.Scan()
+				if tokenKind == parser.EOF {
+					break
+				}
+
+				token := parser.NewToken(tokenKind, literal)
+
+				fmt.Println(token.String())
+			}
+		}
+	}
 }
 
 func repl() {
-  fmt.Print("RAVN 0.1 LEXER REPL >>> ")
+	fmt.Print("RAVN 0.1 LEXER REPL >>> ")
 }
 
 func unknownCommand(text string) {
-  fmt.Println("Unknown command -> ", text)
+	fmt.Println("Unknown command -> ", text)
 }
 
 func clearScreen() {
-    cmd := exec.Command("clear")
-    cmd.Stdout = os.Stdout
-    cmd.Run()
+	fmt.Print("\033[H\033[2J")
 }
-
